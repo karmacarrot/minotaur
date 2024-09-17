d4c8c4dacc911a3d206aff029d9680b64db91a2b
Mark Luxon <mark.luxon@jet2.com>
2024-09-17 15:28:50 +0100

    fix: re-exporting components

--- packages/minotaur-chess-react/src/src/index.ts
+++ packages/minotaur-chess-react/src/src/index.ts
@@ -1,2 +1,12 @@
 export * from "./hooks/useMinotaur";
+export * from "./components/BitBoardGenerator/BitBoardGenerator";
+export * from "./components/BitBoardGenerator/CheckBoxBoard";
+export * from "./components/ChessBoard/ChessBoard";
+export * from "./components/ChessGame/ChessGame";
+export * from "./components/DataDump/DataDump";
+export * from "./components/EvaluationBar/EvaluationBar";
+export * from "./components/GameControls/GameControls";
+export * from "./components/GameStatus/GameStatus";
+export * from "./components/Tile/Tile";
+
 export * from "./definitions";

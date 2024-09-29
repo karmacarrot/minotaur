740485c30c4671be8ce708d2eaf92932ed18e42c
Mark Luxon <mark.luxon@jet2.com>
2024-09-29 18:13:57 +0100

    feat: adding touch events

--- packages/minotaur-chess-react/src/src/components/Tile/Tile.tsx
+++ packages/minotaur-chess-react/src/src/components/Tile/Tile.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import styled from "styled-components";
+import { useEffect, useRef } from "react";
 import { MinotaurConfig, Piece } from "@karmacarrot/minotaur-chess-engine";
 
 const TileContainer = styled.div<{ tileType: string }>`
@@ -18,22 +19,18 @@ const PieceContainer = styled.div<{ threat: boolean; pieceImage: string }>`
   height: 50px;
   width: 50px;
   background-image: url(${({ pieceImage }) => pieceImage});
-  hover {
-    cursor: grab;
-  }
-  active {
+  cursor: grab;
+
+  &:active {
     cursor: grabbing;
   }
+
   pointer-events: auto;
+
   ${({ threat }) =>
     threat &&
     `
-    background-repeat: no-repeat;
-    background-position: center;
     background-color: rgb(134, 12, 12);
-    background-size: 45px;
-    height: 50px;
-    width: 50px;
   `}
 `;
 
@@ -54,25 +51,70 @@ export function Tile({
   threat: boolean;
   config: MinotaurConfig;
 }) {
-  let pieceImage = "";
-  if (piece && config.pieceImages[piece]) {
-    pieceImage = config.pieceImages[piece];
-  }
+  const pieceImage =
+    piece && config.pieceImages[piece] ? config.pieceImages[piece] : "";
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const handleDragStart = (e: React.DragEvent) => {
+    pickUpPiece(piece, e.pageX, e.pageY);
+  };
+
+  const handleDrag = (e: React.DragEvent) => {
+    movePiece(piece, e.pageX, e.pageY);
+  };
+
+  const handleDragEnd = (e: React.DragEvent) => {
+    putDownPiece(piece, e.pageX, e.pageY);
+  };
+
+  const handleTouchStart = (e: TouchEvent) => {
+    e.preventDefault();
+    const touch = e.touches[0];
+    pickUpPiece(piece, touch!.clientX, touch!.clientY);
+  };
+
+  const handleTouchMove = (e: TouchEvent) => {
+    e.preventDefault();
+    const touch = e.touches[0];
+    movePiece(piece, touch!.clientX, touch!.clientY);
+  };
+
+  const handleTouchEnd = (e: TouchEvent) => {
+    e.preventDefault();
+    const touch = e.changedTouches[0];
+    putDownPiece(piece, touch!.clientX, touch!.clientY);
+  };
+
+  useEffect(() => {
+    const container = containerRef.current;
+
+    if (container) {
+      container.addEventListener("touchstart", handleTouchStart, {
+        passive: false,
+      });
+      container.addEventListener("touchmove", handleTouchMove, {
+        passive: false,
+      });
+      container.addEventListener("touchend", handleTouchEnd, {
+        passive: false,
+      });
+
+      return () => {
+        container.removeEventListener("touchstart", handleTouchStart);
+        container.removeEventListener("touchmove", handleTouchMove);
+        container.removeEventListener("touchend", handleTouchEnd);
+      };
+    }
+  }, [piece, movePiece, pickUpPiece, putDownPiece]);
 
   return (
-    <TileContainer tileType={tiletype}>
+    <TileContainer ref={containerRef} tileType={tiletype}>
       {piece && (
         <div
           draggable
-          onDragStart={(e) => {
-            pickUpPiece(piece, e.pageX, e.pageY);
-          }}
-          onDrag={(e) => {
-            movePiece(piece, e.pageX, e.pageY);
-          }}
-          onDragEnd={(e) => {
-            putDownPiece(piece, e.pageX, e.pageY);
-          }}
+          onDragStart={handleDragStart}
+          onDrag={handleDrag}
+          onDragEnd={handleDragEnd}
         >
           <PieceContainer threat={threat} pieceImage={pieceImage} id={piece} />
         </div>
